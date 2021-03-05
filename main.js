353c9d77570a3c2d0675c93204beab2486d9f8b6
city7gor <city7gor@gmail.com>
2021-03-05 14:38:46 +0200

    05.03.2021, noonday

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+let currentItem = 0
+
 document.querySelectorAll('.questions__item').forEach((i, index) => {
   i.addEventListener('click', () => {
-    document.querySelectorAll('.questions__item').forEach((item) => {
-      item.classList.remove('active')
+    document.querySelectorAll('.questions__item').forEach((item, itemIndex) => {
+      if (itemIndex !== index) {
+        item.classList.remove('active')
+      }
     })
-    document.querySelectorAll('.questions__item')[index].classList.toggle('active')
+    if (document.querySelectorAll('.questions__item')[index].classList.contains('active')) {
+      document.querySelectorAll('.questions__item')[index].classList.remove('active')
+    } else {
+      if(currentItem !== index) {
+        setTimeout(() => {
+          document.querySelectorAll('.questions__item')[index].classList.add('active')
+        }, 400);
+        console.log(currentItem)
+      } else {
+        document.querySelectorAll('.questions__item')[index].classList.add('active')
+      }
+    }
+    currentItem = index
   })
 })
